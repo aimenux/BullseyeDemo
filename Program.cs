c337cf4f207ce0a77354d53902df7439de8339da
Aymen TROUDI <aymen.troudi@younited-credit.fr>
2020-03-03 23:58:57 +0100

    First commit !

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static Bullseye.Targets;
+
+namespace App
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Target(nameof(Targets.Create), () => Console.WriteLine("Create infra"));
+            Target(nameof(Targets.Deploy), DependsOn(nameof(Targets.Create)), () => Console.WriteLine("Deploy api"));
+            Target(nameof(Targets.Start), DependsOn(nameof(Targets.Deploy)), () => Console.WriteLine("Start api"));
+            Target(nameof(Targets.Stop), DependsOn(nameof(Targets.Start)), () => Console.WriteLine("Stop api"));
+            Target(nameof(Targets.Destroy), DependsOn(nameof(Targets.Stop)), () => Console.WriteLine("Destroy infra"));
+            Target(nameof(Targets.Default), DependsOn(nameof(Targets.Create), nameof(Targets.Destroy)));
+            RunTargetsWithoutExiting(args);
+            Console.WriteLine("Press any key to exit !");
+            Console.ReadKey();
+        }
+
+        public enum Targets
+        {
+            Create,
+            Deploy,
+            Start,
+            Stop,
+            Destroy,
+            Default
+        }
+    }
+}
