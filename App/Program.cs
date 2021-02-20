afd67ffbfbd814fafe3f0d7327dc52f9e4c5d704
aimenux <devplusdev@gmail.com>
2021-02-20 21:23:36 +0100

    Upgrade to net 5.0

--- App/Program.cs
+++ App/Program.cs
@@ -7,11 +7,11 @@ namespace App
     {
         public static void Main(string[] args)
         {
-            Target(nameof(Targets.Create), () => Console.WriteLine("Create infra"));
-            Target(nameof(Targets.Deploy), DependsOn(nameof(Targets.Create)), () => Console.WriteLine("Deploy api"));
-            Target(nameof(Targets.Start), DependsOn(nameof(Targets.Deploy)), () => Console.WriteLine("Start api"));
-            Target(nameof(Targets.Stop), DependsOn(nameof(Targets.Start)), () => Console.WriteLine("Stop api"));
-            Target(nameof(Targets.Destroy), DependsOn(nameof(Targets.Stop)), () => Console.WriteLine("Destroy infra"));
+            Target(nameof(Targets.Create), () => Console.WriteLine("Create infrastructure"));
+            Target(nameof(Targets.Deploy), DependsOn(nameof(Targets.Create)), () => Console.WriteLine("Deploy web api"));
+            Target(nameof(Targets.Start), DependsOn(nameof(Targets.Deploy)), () => Console.WriteLine("Start web api"));
+            Target(nameof(Targets.Stop), DependsOn(nameof(Targets.Start)), () => Console.WriteLine("Stop web api"));
+            Target(nameof(Targets.Destroy), DependsOn(nameof(Targets.Stop)), () => Console.WriteLine("Destroy infrastructure"));
             Target(nameof(Targets.Default), DependsOn(nameof(Targets.Create), nameof(Targets.Destroy)));
             RunTargetsWithoutExiting(args);
             Console.WriteLine("Press any key to exit !");
--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-![.NET Core](https://github.com/aimenux/BullseyeDemo/workflows/.NET%20Core/badge.svg)
+![.NET Core](https://github.com/aimenux/BullseyeDemo/workflows/.NET%205/badge.svg)
 # BullseyeDemo
 ```
 Using Bullseye to simulate api lifecycle management tasks
 ```
 
-In this demo, i m using [Bullseye](https://github.com/adamralph/bullseye/) to simulate some management tasks.
+In this demo, i m using [Bullseye](https://github.com/adamralph/bullseye/) to simulate some fake management tasks.
 
 > How to run ?
->> `Show help :` App.exe -h
 >
->> `Show targets :` App.exe -l
+> :writing_hand: `Show help` : App.exe -h
 >
->> `Run all targets :` App.exe
+> :writing_hand: `Show targets` : App.exe -l
 >
->> `Run create target :` App.exe Create
+> :writing_hand: `Run all targets` : App.exe
+>
+> :writing_hand: `Run create target` : App.exe Create
+>
+> ![DefaultTargetScreen](Screenshots/DefaultTargetScreen.png)
+>
 
-**`Tools`** : vs19, net core 3.1
+**`Tools`** : vs19, net 5.0
